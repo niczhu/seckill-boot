55e261253a8403bc67158de8ccc66873cdfb71cc
zhuhaipeng <dell@qq.com>
2019-09-24 17:18:09 +0800

    update

--- src/main/java/com/seckill/seckillboot/dal/mapper/SeckillDao.java
+++ src/main/java/com/seckill/seckillboot/dal/mapper/SeckillDao.java
@@ -2,10 +2,12 @@ package com.seckill.seckillboot.dal.mapper;
 
 
 import com.seckill.seckillboot.dal.bean.SeckillBean;
+import org.mybatis.spring.annotation.MapperScan;
 
 import java.util.List;
 import java.util.Map;
 
+@MapperScan
 public interface SeckillDao {
 
     List<SeckillBean> search(Map<String,Object> params);
